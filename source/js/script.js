0f8c3e3e5f5bd1be0994d25b09350ca7335eb47c
Ekaterina <ektregubova@gmail.com>
2022-05-16 15:46:47 +0500

    правки по замечаниям

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let mapImage = document.querySelector('.map__image');
+let mapIframe = document.querySelector('.map__iframe');
 
+mapImage.classList.add('map__image--nojs');
+mapIframe.classList.add('map__iframe--js');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
